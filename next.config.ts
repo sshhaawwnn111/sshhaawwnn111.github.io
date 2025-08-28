707558004902bd5da64725dba320c824add035f3
Shawn <sshhaawwnn111@gmail.com>
2025-08-28 12:17:23 -0400

    github action to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "My-Portfolio"; // change if your repo name is different
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   /* config options here */
 };
 
