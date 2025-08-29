9fca558b5549e150bcb561c322c9e08dfced2bd5
Shawn <sshhaawwnn111@gmail.com>
2025-08-29 16:01:03 -0400

    Deploy at root

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -46,7 +46,7 @@ export default function ContactSection() {
 					className="flex flex-col sm:flex-row items-center justify-center gap-4 mb-8"
 				>
 					<a
-						href="mailto:john@example.com"
+						href="mailto:hwang324@jh.edu"
 						className="group flex items-center gap-2 px-6 py-3 bg-white/5 rounded-full hover:bg-white/10 transition-colors w-full sm:w-auto"
 					>
 						<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
@@ -94,7 +94,7 @@ export default function ContactSection() {
 							<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
 						</svg>
 					</a>
-					<a 
+					{/* <a 
 					href="https://twitter.com" 
 					target="_blank" 
 					rel="noopener noreferrer" 
@@ -102,7 +102,7 @@ export default function ContactSection() {
 						<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
 							<path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
 						</svg>
-					</a>
+					</a> */}
 					<a
 					href="https://www.facebook.com/wang.xuan.67205/"
 					target="_blank"
