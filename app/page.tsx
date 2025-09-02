eee61d3ec196d59165af752a58e959f4e581a2f7
Shawn <sshhaawwnn111@gmail.com>
2025-09-01 20:46:35 -0400

    Deploy at root

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import ProjectsSection from './components/ProjectsSection';
 import SkillsSection from './components/SkillsSection';
 import ContactSection from './components/ContactSection';
 import AboutSection from './components/AboutSection';
+import ExperienceSection from './components/ExperienceSection';
 
 export default function MinimalModernPortfolio() {
 	return (
 		<div className="min-h-screen bg-black text-white">
 			<HeroSection />
 			<AboutSection />
+			<ExperienceSection />
 			<ProjectsSection />
 			<SkillsSection />
 			<ContactSection />
